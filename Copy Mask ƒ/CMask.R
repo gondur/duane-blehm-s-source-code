baf9950760ef87017d8e93024ea57e8ed5ed37a8
za-mi <michael.zapf@kit.edu>
2018-08-08 18:50:29 +0200

    create

---
+++ Copy Mask ƒ/CMask.R
@@ -0,0 +1,1 @@
+* CMask.RCMask.rsrcTYPE MENU ,1\14 ,256File OffScreen.. Quit* will include Pict resources file built with ResEdit and ScrapBookINCLUDE CMask/rsrc
---
+++ StuntCopter ƒ/Anim.R
@@ -0,0 +1,1 @@
+* Anim.RAnim.rsrcTYPE MENU ,1\14 ,256File OffScreen.. Quit* will include Pict resource file built with ResEdit and ScrapBookINCLUDE Anim/rsrc
